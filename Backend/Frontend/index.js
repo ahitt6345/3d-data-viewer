1105ff683eae0235fc0dcd256a90faeaff9e6a62
Alex <ahitt63@gmail.com>
2024-06-03 11:34:24 -0700

    Restructured the server directory in order to cause less problems linking the Frontend to the back end. We can start taking a crack at the data soon.

--- Backend/Frontend/index.js
+++ Backend/Frontend/index.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from '/ext/three.module.js';
 
 // render a sphere
 const scene = new THREE.Scene();
--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+var fs = require('fs');
 var path = require('path');
+
+
 // simple file hosting
 // frontend files are in ../Frontend
-app.get('*', (req, res) => {
-    var dir = path.join(__dirname, '../');
-    console.log(dir + req.url);
-    res.sendFile(dir + req.url);
+app.get('*', (req, res) => { // This is a catch all for all requests, however we need to restructure this so that it only serves files from the frontend folder.
+  var dir = path.join(__dirname, './Frontend'); 
+  
+  console.log(dir + req.url);
+  res.sendFile(dir + req.url);
 });
-
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
