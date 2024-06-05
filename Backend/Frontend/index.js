eed54f8fe414358e238649fca7ad34ae9c5c5e31
Alex <ahitt63@gmail.com>
2024-06-04 20:45:32 -0700

    Noticed that the movement controls were not related to where the camera was facing, fixed that here. Also added more to the company and application spheres.

--- Backend/Frontend/index.js
+++ Backend/Frontend/index.js
@@ -1,20 +1,12 @@
 import * as THREE from '/ext/three.module.js';
-import {
-    PointerLockControls
-} from '/ext/PointerLockControls.js';
+import { PointerLockControls } from '/ext/PointerLockControls.js';
+import { TextGeometry } from '/ext/TextGeometry.js';
 // render a sphere
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.SphereGeometry(5, 32, 32);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xffff00
-});
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
 
 camera.position.z = 10;
 
@@ -85,36 +77,69 @@ function animate() {
     var time = performance.now();
     var delta = (time - prevTime) / 30;
 
-    if (isPressed['W']) {
-        camera.position.z -= 0.1;
+    const direction = controls.getDirection(new THREE.Vector3(0, 0, -1)).clone();
+    const speed = 0.1;
+
+    if (isPressed['W']) { // forward
+        camera.position.add(direction.multiplyScalar(speed));
     }
-    if (isPressed['S']) {
-        camera.position.z += 0.1;
+    if (isPressed['S']) { // backward
+        camera.position.add(direction.multiplyScalar(-speed));
     }
-    if (isPressed['A']) {
-        camera.position.x -= 0.1;
+    if (isPressed['A']) { // left
+        const left = new THREE.Vector3(-1, 0, 0);
+        const leftDirection = left.applyQuaternion(controls.getObject().quaternion);
+        camera.position.add(leftDirection.multiplyScalar(speed));
     }
-    if (isPressed['D']) {
-        camera.position.x += 0.1;
+    if (isPressed['D']) { // right
+        const right = new THREE.Vector3(1, 0, 0);
+        const rightDirection = right.applyQuaternion(controls.getObject().quaternion);
+        camera.position.add(rightDirection.multiplyScalar(speed));
     }
-    if (isPressed['Space']) {
-        camera.position.y += 0.1;
+    if (isPressed['Space']) { // up
+        camera.position.y += speed;
     }
-    if (isPressed['C']) {
-        camera.position.y -= 0.1;
+    if (isPressed['C']) { // down
+        camera.position.y -= speed;
     }
     renderer.render(scene, camera);
     prevTime = time;
 }
 
 animate();
-var CompanySphere = function() {
-    
+var CompanySphere = function(x,y,z,radius, companyData) {
+    this.sphere = new THREE.Mesh(new THREE.SphereGeometry(radius, 32, 32), new THREE.MeshBasicMaterial({color: 0xffff00}));
+    this.sphere.position.set(x,y,z);
+    // write the company name on the sphere using threejs text
+    var text = new TextGeometry(companyData.company, {
+        //font: 'helvetiker',
+        size: 1,
+        height: 0.1
+    });
+    var textMaterial = new THREE.MeshBasicMaterial({color: 0x000000});
+    var textMesh = new THREE.Mesh(text, textMaterial);
+    textMesh.position.set(x, y, z);
+    scene.add(textMesh);
+    this.companyData = companyData;
+    scene.add(this.sphere);
 };
-var ApplicationSphere = function() {
+var ApplicationSphere = function(x,y,z,radius, application) {
     this.companies = [];
-
+    this.sphere = new THREE.Mesh(new THREE.SphereGeometry(radius, 32, 32), new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 0.5, transparent: true}));
+    this.sphere.position.set(x,y,z);
+    this.application = application;
+    scene.add(this.sphere);
 };
+
+ApplicationSphere.prototype.addCompany = function(companyData) { // add a CompanySphere to the ApplicationSphere located somewhere inside of the sphere
+    var applicationSphereRadius = this.sphere.geometry.parameters.radius;
+    var x = this.sphere.position.x + (Math.random() * (applicationSphereRadius * 2) - applicationSphereRadius);
+    var y = this.sphere.position.y + (Math.random() * (applicationSphereRadius * 2) - applicationSphereRadius);
+    var z = this.sphere.position.z + (Math.random() * (applicationSphereRadius * 2) - applicationSphereRadius);
+    var newCompanySphere = new CompanySphere(x, y, z, (companyData.mosaic / 1000) * 5, companyData);
+    this.companies.push(newCompanySphere);
+    
+}
 var processData = function(data){
     var applications = {}; // This will be used to store the unique applications
     for (var i = 0; i < data.length; i++) {
@@ -122,7 +147,27 @@ var processData = function(data){
     }
     applications = Object.keys(applications); // Get the array of unique applications
     console.log(applications);
+    var applicationSpheres = [];
+    for (var i = 0; i < applications.length; i++) {
+        var newApplicationSphere = new ApplicationSphere(
+            Math.random() * 100 - 50,
+            Math.random() * 100 - 50,
+            Math.random() * 100 - 50,
+            10,
+            applications[i]
+        );
+        applicationSpheres.push(newApplicationSphere);
+    }
 
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < applicationSpheres.length; j++) {
+            if (data[i].applications === applicationSpheres[j].application) {
+                applicationSpheres[j].addCompany(data[i]);
+                break;
+            }
+        }
+    }
+    
 
 };
 // connect to the server
@@ -135,6 +180,7 @@ socket.on('connect', () => {
     socket.on('data', (data) => {
         if (data === 'End of data') {
             console.log('All data received');
+            processData(localData);
         } else {
             // Handle the received data chunk
             localData = localData.concat(data);
